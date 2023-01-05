a50a069104d980621d3adee87d715c3bcd4c8f4a
Jill Tsai <jilljane32@gmail.com>
2023-01-05 18:26:01 +0800

    gototop修正完畢

--- js/js01.js
+++ js/js01.js
@@ -1,33 +1,48 @@
 $(function() {  
-$(window).scroll(function(){
-   object_scroll();	
-});
-function object_scroll(){	
-    if ($(window).scrollTop()>50){
-        $('.fixed_bar').css({display:'flex'});
-    }else {
-        $('.fixed_bar').css({display:'none'});
-    }
+    function backgo(){
+        var all_h=$(window).scrollTop() + $(window).height();
+        var obj_h=$('footer').outerHeight()+10;
+        var foot_top= $('footer').offset().top;
+        var bottom_ad_h=$('.flexd_bottom_ad').outerHeight();
     
-    if ($(window).scrollTop()>150){   
-        $('.fixed_bar').css({top:0});
-        $('#back').fadeIn(); 
-        $('#back').css({ 'bottom': '25px' });
-        // console.log("scrollTop:"+$(window).scrollTop());   
-        // console.log("height:"+$(window).height()); 
-        let window_hight=$(window).height();
-        if ($(window).scrollTop()>(window_hight-110)){  
-            //alert(window_hight-110);
-            $('#back').css({ 'bottom': '60px' });
-        }        
-    } else {
-        $('.fixed_bar').css({top:'-100%'});
-        $('#back').fadeOut(); 
-        $('#back').css({ 'bottom': '-100px' });
-    }
+        if ($(window).scrollTop()>50){
+            $("#back").fadeIn(200);
+            $("nav").css({
+                'background-color':'rgba(0,0,0,0.7)',
+                'backdrop-filter': 'blur(20px)'
+            });
+        }else{
+            $("#back").fadeOut(200);
+            $("nav").css({
+                'background-color':'rgba(0,0,0,1)',
+                'backdrop-filter': 'blur(20px)'
+            });
+        }
     
-
-}    
+        if ($(window).width()>767){
+            $(".flexd_bottom_ad").hide();
+            if(all_h > foot_top) {
+                $("#back").css({"bottom":obj_h-40});
+            }else{
+                $("#back").css({"bottom":"20px"});
+            }
+        }else{
+            if ($(window).scrollTop()>50){
+                $(".flexd_bottom_ad").show();
+            }else{
+                $(".flexd_bottom_ad").hide();
+            }
+            if(all_h > foot_top) {
+                $("#back").css({"bottom":obj_h-40,"transition":"0.3s"});
+                // $(".flexd_bottom_ad").css({"bottom":obj_h-10,"transition":"0.3s"});
+    
+            }else{      
+                $("#back").css({"bottom":"20px","transition":"0.3s"});
+               // $(".flexd_bottom_ad").css({"bottom":"0px","transition":"0.3s"});
+            }
+        }
+    } 
+    $(window).scroll(function(){backgo();}); 
 /*gotop*/
     var $el = $(scrollableElement('html', 'body'));
     var speed = 550;
